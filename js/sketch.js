671f8611470b6f78700e1f3a82bb09d102793666
Sam B <samuel.barry.b@hotmail.com>
2020-01-28 11:52:17 +0800

    Initial Commit

---
+++ js/sketch.js
@@ -0,0 +1,12 @@
+var canvasWidth = 800;
+var canvasHeight = 800;
+
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(canvasWidth, canvasHeight);
+}
+function draw() {
+    backround(0);
+}
